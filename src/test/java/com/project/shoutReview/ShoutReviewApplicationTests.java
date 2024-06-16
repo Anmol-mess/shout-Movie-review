9e45dc496c7ed17062762c556dba21ee2883a59c
DESKTOP-ECNTMQD\acer <anmolgoel4567@gmail.com>
2024-06-16 13:31:38 +0530

    Here we start...!

---
+++ src/test/java/com/project/shoutReview/ShoutReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.shoutReview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoutReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
